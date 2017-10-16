d4aad0ae63271023dd5382efee2baa868ba7b078
yleprince <yrieix.leprince@telecom-paristech.fr>
2017-10-16 10:30:24 +0200

    adding question 42

--- systemesRepartis/src/systemesRepartis/MASTER.java
+++ systemesRepartis/src/systemesRepartis/MASTER.java
@@ -1,25 +1,39 @@
 package systemesRepartis;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
 
 public class MASTER {
-    public static void main(String[] args) throws IOException {
-    	
-    	//ProcessBuilder pb1 = new ProcessBuilder("ls", "-al", "/tmp");
-    	
-    	ProcessBuilder pb1 = new ProcessBuilder("java", "-jar", "/home/bud/Downloads/tmp/inf727_system_repartis/slave.jar");
-    	Process pro = pb1.start();
-    	BufferedReader reader = new BufferedReader(new InputStreamReader(pro.getInputStream()));
-        StringBuilder builder = new StringBuilder();
-        String line = null;
+	public static void main(String[] args) throws Exception {
+		Process p = Runtime.getRuntime().exec("ls /jeslmkjs");
+		inheritIO(p.getInputStream(), System.out);
+		inheritIO(p.getErrorStream(), System.err);
 
-        while ( (line = reader.readLine()) != null) {
-            builder.append(line);
-            builder.append(System.getProperty("line.separator"));
-        }
+	}
 
-        String result = builder.toString();
-        System.out.println("Reader : " + result);
-    }
+	private static void inheritIO(final InputStream src, final PrintStream dest) {
+		new Thread(new Runnable() {
+			private Scanner sc;
+
+			public void run() {
+				sc = new Scanner(src);
+				while (sc.hasNextLine()) {
+					dest.println(sc.nextLine());
+				}
+			}
+		}).start();
+	}
 }
+
+/*
+ * public static void main(String[] args) throws Exception { Process p =
+ * Runtime.getRuntime().exec("cmd /c dir"); inheritIO(p.getInputStream(),
+ * System.out); inheritIO(p.getErrorStream(), System.err);
+ * 
+ * }
+ * 
+ * 
+ * 
+ * 
+ */
