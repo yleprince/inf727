0c6fedc89f5d93d0729f5290802e30891769fe15
yleprince <yrieix.leprince@telecom-paristech.fr>
2017-10-16 10:03:56 +0200

    adding comment

--- systemesRepartis/src/systemesRepartis/SLAVE.java
+++ systemesRepartis/src/systemesRepartis/SLAVE.java
@@ -3,6 +3,7 @@ package systemesRepartis;
 public class SLAVE {
     public static void main(String[] args) {
     	// comment test 2
+    	// Comment test 3
         int result = 3 + 5;
         System.out.print("3 + 5 = ");
         System.out.print(result);
