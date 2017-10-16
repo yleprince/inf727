4f9022401ba348f9d7324113d4cbece356990d58
yleprince <yrieix.leprince@telecom-paristech.fr>
2017-10-16 11:45:52 +0200

    Q45 fin du cours

--- systemesRepartis/src/systemesRepartis/MASTER_Q45.java
+++ systemesRepartis/src/systemesRepartis/MASTER_Q45.java
@@ -1,5 +1,26 @@
 package systemesRepartis;
 
-public class MASTER_Q45 {
+import java.io.IOException;
 
+public class MASTER_Q45 {
+	public static void main(String[] args) throws IOException {
+
+		// ProcessBuilder pb1 = new ProcessBuilder("ls", "-al", "/tmp");
+		ProcessBuilder pb1 = new ProcessBuilder("java", "-jar",
+				"/home/bud/Downloads/tmp/inf727_system_repartis/slave.jar");
+
+		// TODO: RELIRE LA QUESTION (PARTIE ASTUCE EN BAS DE Q45)
+		// ArrayBlockingQueue
+		/*
+		 * int timeout = 2000; long startTime = System.currentTimeMillis();
+		 * 
+		 * THREAD_STD std_thread = new THREAD_STD(pb1); THREAD_ERROR error_thread = new
+		 * THREAD_ERROR(pb1);
+		 * 
+		 * while (System.currentTimeMillis() < startTime + timeout) {
+		 * std_thread.start(); error_thread.start(); }
+		 * 
+		 * std_thread.stop(); error_thread.stop();
+		 */
+	}
 }
