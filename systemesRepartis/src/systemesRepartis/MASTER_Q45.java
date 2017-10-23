a1f26c355298e1386451975b3abf3029a4388519
yleprince <yrieix.leprince@telecom-paristech.fr>
2017-10-23 09:21:15 +0200

    Q45 avancée, le master est presque fonctionnel, il reste à faire les threads

--- systemesRepartis/src/systemesRepartis/MASTER_Q45.java
+++ systemesRepartis/src/systemesRepartis/MASTER_Q45.java
@@ -1,26 +1,48 @@
 package systemesRepartis;
 
 import java.io.IOException;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 public class MASTER_Q45 {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, InterruptedException {
 
-		// ProcessBuilder pb1 = new ProcessBuilder("ls", "-al", "/tmp");
-		ProcessBuilder pb1 = new ProcessBuilder("java", "-jar",
-				"/home/bud/Downloads/tmp/inf727_system_repartis/slave.jar");
+		float timeout = (float) 2.0;
+		ProcessBuilder pb45 = new ProcessBuilder("java", "-jar", "/home/bud/Downloads/tmp/slave.jar");
+		getResponse(pb45, timeout);
+		
 
-		// TODO: RELIRE LA QUESTION (PARTIE ASTUCE EN BAS DE Q45)
-		// ArrayBlockingQueue
-		/*
-		 * int timeout = 2000; long startTime = System.currentTimeMillis();
-		 * 
-		 * THREAD_STD std_thread = new THREAD_STD(pb1); THREAD_ERROR error_thread = new
-		 * THREAD_ERROR(pb1);
-		 * 
-		 * while (System.currentTimeMillis() < startTime + timeout) {
-		 * std_thread.start(); error_thread.start(); }
-		 * 
-		 * std_thread.stop(); error_thread.stop();
-		 */
 	}
+
+	public static String getResponse(ProcessBuilder pb, float timeout) throws IOException, InterruptedException {
+		Process p = pb.start();
+
+		BlockingQueue<String> blocking_queue = new ArrayBlockingQueue<String>(1024);
+		THREAD_STD t = new THREAD_STD(p, blocking_queue); // Creation du thread reader std
+		t.start();
+		String response = blocking_queue.poll(timeout, TimeUnit.SECONDS);
+
+		BlockingQueue<String> blocking_queue_error = new ArrayBlockingQueue<String>(1024);
+		THREAD_ERROR te = new THREAD_ERROR(p, blocking_queue_error); // Creation du thread reader err
+		te.start();
+		String responseErr = blocking_queue_error.poll(timeout, TimeUnit.SECONDS);
+
+		if (response == null && responseErr == null) {
+			System.err.println("Process killed by timeout");
+		} else if (response != null && responseErr.length() == 0) {
+			System.out.println("Response : " + response);
+			return response;
+		} else if (response.length() == 0 && responseErr != null) {
+			System.err.println("Error : " + responseErr);
+		} else {
+			System.out.println("Response : " + response);
+			System.err.println("Error : " + responseErr);
+		}
+		p.destroy();
+		t.interrupt();
+		te.interrupt();
+		return "null";
+	}
+
 }
