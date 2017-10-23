a7208273e754b7838a9a1115a9b0a6eeea401950
yleprince <yrieix.leprince@telecom-paristech.fr>
2017-10-23 12:22:35 +0200

    Code cleaned

--- systemesRepartis/src/systemesRepartis/MASTER_PART_8.java
+++ systemesRepartis/src/systemesRepartis/MASTER_PART_8.java
@@ -16,64 +16,62 @@ public class MASTER_PART_8 {
 
 		// long timeout = 2; // Test 1
 
-		//long timeout = 15; // Test 2
+		// long timeout = 15; // Test 2
 		// ProcessBuilder pb45 = new ProcessBuilder("java", "-jar",
 		// "/home/bud/Downloads/tmp/slave.jar");
 
-		//ProcessBuilder pb45 = new ProcessBuilder("java", "-jar", "/home/bud/Downloads/tmp/slave_Q45_test3.jar"); // Test3
+		// ProcessBuilder pb45 = new ProcessBuilder("java", "-jar",
+		// "/home/bud/Downloads/tmp/slave_Q45_test3.jar"); // Test3
 		// getResponse(pb45, timeout);
 
 		String computerList = "/home/bud/Documents/s1/inf727/computersOn.txt";
-		/*
-		ProcessBuilder pb47 = new ProcessBuilder("java", "-jar", "/home/bud/Downloads/tmp/slave_Q45_test3.jar"); // 
-		ProcessBuilder pb5 = new ProcessBuilder("ssh", "-o", "StrictHostKeyChecking=no", "cbelinguier@" + namePc, "mkdir", "-p", "/tmp/cbe
-				linguier");
-*/
+
 		deploy(computerList);
-		
+
 	}
 
 	public static void deploy(String computerListFilename) throws IOException, InterruptedException {
-		
+
 		System.out.println("Enter Deploy");
 		ArrayList<String> computersUsables = deployable(computerListFilename);
 		System.out.println("computersUsables" + computersUsables);
 		String response_mkdir;
 		String response_scp;
-		
+
 		System.out.println("Loop Deploy");
-		for (String pc: computersUsables) {
-			ProcessBuilder pb_mkdir = new ProcessBuilder("ssh",  "-o", "StrictHostKeyChecking=no", "yleprince@" + pc, "mkdir", "-p", "/tmp/yleprince1/");
+		for (String pc : computersUsables) {
+			ProcessBuilder pb_mkdir = new ProcessBuilder("ssh", "-o", "StrictHostKeyChecking=no", "yleprince@" + pc,
+					"mkdir", "-p", "/tmp/yleprince1/");
 			response_mkdir = getResponse(pb_mkdir, 5);
-			System.out.println("\tDeployed on: "+ pc);
-			//if (response_mkdir.length()==0) {System.out.println("response_mkdir" + response_mkdir);}
-			//System.out.println(response_mkdir);
-			
-			ProcessBuilder pb_scp = new ProcessBuilder("scp",  "/home/bud/Downloads/tmp/slave.jar", "yleprince@" + pc +":/tmp/yleprince1/");
+			System.out.println("\tDeployed on: " + pc + response_mkdir);
+
+			ProcessBuilder pb_scp = new ProcessBuilder("scp", "/home/bud/Downloads/tmp/slave.jar",
+					"yleprince@" + pc + ":/tmp/yleprince1/");
 			response_scp = getResponse(pb_scp, 5);
-			System.out.println("\tCopied on: "+ pc);
-			//if (response_scp.length()==0) {System.out.println("response_scp" + response_scp);}
-			
+			System.out.println("\tCopied on: " + pc + response_scp);
+
+
 			System.out.println();
 		}
-		
+
 		System.out.print("End deploy");
 	}
-	
+
 	public static ArrayList<String> deployable(String filename) throws IOException, InterruptedException {
 
 		ArrayList<String> computersNames = readFileLineByLine(filename);
 		ArrayList<String> computersUsables = new ArrayList<String>();
 		String response;
 		for (String pc : computersNames) {
-			ProcessBuilder pb46 = new ProcessBuilder("ssh",  "-o", "StrictHostKeyChecking=no", "yleprince@" + pc, "hostname");
-            response = getResponse(pb46, 5);
-            if (response != null) {
-            	computersUsables.add(pc);
-            }
+			ProcessBuilder pb46 = new ProcessBuilder("ssh", "-o", "StrictHostKeyChecking=no", "yleprince@" + pc,
+					"hostname");
+			response = getResponse(pb46, 5);
+			if (response != null) {
+				computersUsables.add(pc);
+			}
 		}
 		return computersUsables;
-				
+
 	}
 
 	public static ArrayList<String> readFileLineByLine(String filename) {
@@ -91,7 +89,7 @@ public class MASTER_PART_8 {
 			while ((readLine = b.readLine()) != null) {
 				lines.add(readLine);
 			}
-			
+
 			b.close();
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -115,7 +113,7 @@ public class MASTER_PART_8 {
 		if (response == null && responseErr == null) {
 			System.err.println("Process killed by timeout");
 		} else if (response != null && responseErr.length() == 0) {
-			//System.out.println("Response std : " + response);
+			// System.out.println("Response std : " + response);
 			return response;
 		} else if (response.length() == 0 && responseErr != null) {
 			System.err.println("Error : " + responseErr);
